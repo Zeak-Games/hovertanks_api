a8cdc143ff51aee799b60c9f059c50e81304f7fa
Ignacio Quinteros <i.quinteros@hotmail.com>
2022-01-15 23:38:18 -0300

    Adding guest welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,10 @@
 
                 <div class="card-body">
                     <h4 class="card-title">Hi! Welcome to HoverTanks web dashboard</h4>
+
+                    @guest
+                        <a class="btn btn-sm btn-primary mt-4" href="{{ route('report.create') }}">Send feedback report</a>
+                    @endguest
                 </div>
 
             </div>
