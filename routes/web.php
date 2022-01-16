dbc6fde91eafd14490f1a3f80235c15bd7e6eea9
Ignacio Quinteros <i.quinteros@hotmail.com>
2022-01-15 22:07:47 -0300

    Adding middleware to feedback report admin

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,5 @@ Auth::routes(['register' => false]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('/reports', FeedbackReportAdmin::class)
+    ->middleware('auth')
     ->only(['index', 'show']);
